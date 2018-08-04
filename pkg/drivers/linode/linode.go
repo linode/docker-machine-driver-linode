a8e0066252b648fa42305be568ea97c1919e99dc
Marques Johansson <marques@displague.com>
2018-08-04 08:19:31 -0400

    release: v0.0.8 docker-machine rm fix for removed linode instances

--- pkg/drivers/linode/linode.go
+++ pkg/drivers/linode/linode.go
@@ -41,7 +41,7 @@ type Driver struct {
 
 const (
 	// VERSION represents the semver version of the package
-	VERSION               = "0.0.7"
+	VERSION               = "0.0.8"
 	defaultSSHPort        = 22
 	defaultSSHUser        = "root"
 	defaultInstanceImage  = "linode/ubuntu18.04"

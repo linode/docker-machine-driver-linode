5081e6e965ae6df1c822412266def1cd06bf9ffc
Marques Johansson <marques@displague.com>
2018-08-04 10:52:14 -0400

    fix: add a test to verify NewDriver behavior

--- pkg/drivers/linode/linode_test.go
+++ pkg/drivers/linode/linode_test.go
@@ -1,1 +1,25 @@
-package linode_test
+package linode
+
+import (
+	"testing"
+
+	"github.com/docker/machine/libmachine/drivers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetConfigFromFlags(t *testing.T) {
+	driver := NewDriver("", "")
+
+	checkFlags := &drivers.CheckDriverOptions{
+		FlagsValues: map[string]interface{}{
+			"linode-token":     "PROJECT",
+			"linode-root-pass": "ROOTPASS",
+		},
+		CreateFlags: driver.GetCreateFlags(),
+	}
+
+	err := driver.SetConfigFromFlags(checkFlags)
+
+	assert.NoError(t, err)
+	assert.Empty(t, checkFlags.InvalidFlags)
+}
